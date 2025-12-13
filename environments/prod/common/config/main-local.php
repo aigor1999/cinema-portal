4063e94506398b4f7c96180f8ba88083d679981d
aigor1999 <aigor200799@gmail.com>
2025-12-13 23:54:22 +0300

    Удалены ненужные файлы

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -1,17 +1,5 @@
 <?php
 
 return [
-    'components' => [
-        'db' => [
-            'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
-        ],
-        'mailer' => [
-            'class' => \yii\symfonymailer\Mailer::class,
-            'viewPath' => '@common/mail',
-        ],
-    ],
+
 ];
