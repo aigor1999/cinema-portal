194b2a30468f710c8beaba53f16464731de4fbf4
aigor1999 <aigor200799@gmail.com>
2025-12-16 16:34:12 +0300

    Перенос параметров БД из main.php в main-local.php.

--- common/config/main.php
+++ common/config/main.php
@@ -11,12 +11,5 @@ return [
         'cache' => [
             'class' => \yii\caching\FileCache::class,
         ],
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'pgsql:host=localhost;dbname=cinema_portal',
-            'username' => 'postgres',
-            'password' => '11111111',
-            'charset' => 'utf8',
-        ],
     ],
 ];
--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
-
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'pgsql:host=localhost;dbname=cinema_portal',
+            'username' => 'postgres',
+            'password' => '11111111',
+            'charset' => 'utf8',
+        ],
+    ],
 ];
